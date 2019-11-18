6e13e9e48c03fe95ed8458c0a11ba19656ad19f6
CristianAlfaro <0016797@uca.edu.sv>
2019-11-17 18:30:51 -0600

    ya esta todo

---
+++ server/config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url': 'mongodb://localhost/rimurudatabase2'
+};
---
+++ server/models/user.js
@@ -0,0 +1,27 @@
+const mongoose  = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+const userSchema = new mongoose.Schema({
+    local: {
+        usuario: String,
+        password: String,
+        email: {
+            type: String,
+            unique: true,
+            index: true
+        },
+        admin: Boolean
+    }
+});
+
+
+userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+
+};
+
+userSchema.methods.validatePassword = function(password){
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
